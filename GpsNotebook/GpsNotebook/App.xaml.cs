52b7277d507dac7d9ca12516d1df445d23a24cb7
RuslanBurlai <ruslanburlai@gmail.com>
2021-03-30 06:05:32 -0700

    Create Xamarin.Forms project and added prism.Unity.Forms.

---
+++ GpsNotebook/GpsNotebook/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using GpsNotebook.View;
+using GpsNotebook.ViewModel;
+using Prism.Ioc;
+using Prism.Unity;
+using Xamarin.Forms;
+
+namespace GpsNotebook
+{
+    public partial class App : PrismApplication
+    {
+        public App()
+        {
+        }
+
+        #region -- Ovverides --
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            //Navigation
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync($"{nameof(MainPage)}");
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+
+        #endregion
+    }
+}
---
+++ GpsNotebook/GpsNotebook/View/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace GpsNotebook.View
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
