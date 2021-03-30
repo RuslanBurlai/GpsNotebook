74d3c5d0277f07f9af9e3b3a461a54ca9ec38ed3
RuslanBurlai <ruslanburlai@gmail.com>
2021-03-30 09:00:43 -0700

    added regions

--- GpsNotebook/GpsNotebook/ViewModel/ViewModelBase.cs
+++ GpsNotebook/GpsNotebook/ViewModel/ViewModelBase.cs
@@ -8,16 +8,37 @@ namespace GpsNotebook.ViewModel
 {
     public class ViewModelBase : BindableBase, INavigationAware, IDestructible, IInitialize
     {
+
+        #region -- Public Properties --
+
+        private string _title;
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        #endregion
+
         public ViewModelBase(INavigationService navigationService)
         {
             NavigationService = navigationService;
         }
 
+        #region -- Public Properties --
         protected INavigationService NavigationService { get; private set; }
+
+        #endregion
+
+        #region -- Initialize implementation --
 
         public virtual void Initialize(INavigationParameters parameters)
         {
         }
+
+        #endregion
+
+        #region -- INavigationAware implementation --
 
         public virtual void OnNavigatedFrom(INavigationParameters parameters)
         {
@@ -27,8 +48,14 @@ namespace GpsNotebook.ViewModel
         {
         }
 
+        #endregion
+
+        #region -- IDestructible implementation --
+
         public virtual void Destroy()
         {
         }
+
+        #endregion
     }
 }
